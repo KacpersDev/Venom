537355eca23a4715a17856c7ccba8b174bd76662
kacper <vkacperpl@gmail.com>
2022-02-08 18:38:32 +0100

    Release 0.0.4

--- src/main/java/me/kacpersdev/Venom.java
+++ src/main/java/me/kacpersdev/Venom.java
@@ -3,6 +3,10 @@ package me.kacpersdev;
 import me.kacpersdev.api.API;
 import me.kacpersdev.api.config.ConfigFile;
 import me.kacpersdev.api.inventory.VenomInventory;
+import me.kacpersdev.chatcolor.CC;
+import me.kacpersdev.listener.InventoryClick;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Venom extends JavaPlugin {
@@ -11,10 +15,13 @@ public final class Venom extends JavaPlugin {
     private ConfigFile configFile;
     private VenomInventory venomInventory;
     private API api;
+    private CC cc;
 
     @Override
     public void onEnable() {
         INSTANCE = this;
+        this.pluginListeners();
+
 
     }
 
@@ -27,12 +34,22 @@ public final class Venom extends JavaPlugin {
 
     }
 
+    private void pluginListeners(){
+
+        PluginManager manager = Bukkit.getPluginManager();
+        manager.registerEvents(new InventoryClick(), this);
+    }
+
     public ConfigFile getConfigFile(){
         return this.configFile;
     }
 
     public VenomInventory getVenomInventory(){
         return this.venomInventory;
+    }
+
+    public CC getCC(){
+        return this.cc;
     }
 
     public API getApi(){ return this.api; }
--- src/main/java/me/kacpersdev/api/inventory/VenomInventory.java
+++ src/main/java/me/kacpersdev/api/inventory/VenomInventory.java
@@ -6,10 +6,29 @@ import org.bukkit.inventory.Inventory;
 
 public class VenomInventory {
 
-    public void createInventory(Player player, Inventory inventory, String title, int size){
+    private String name;
+    private int size;
+    private boolean canTakeItems;
+
+    public void createInventory(Player player, Inventory inventory,String title, int size, boolean canTakeItems){
+        this.name = title;
+        this.size = size;
+        this.canTakeItems = canTakeItems;
 
         inventory = Bukkit.createInventory(player, size, title);
 
         player.openInventory(inventory);
+    }
+
+    public String getInventoryName(){
+        return this.name;
+    }
+
+    public Integer getInventorySize(){
+        return this.size;
+    }
+
+    public boolean isCanTakeItems(){
+        return this.canTakeItems;
     }
 }
