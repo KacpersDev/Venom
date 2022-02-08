7a5d3b1018fe67761993a69d19e57d715f60098f
kacper <vkacperpl@gmail.com>
2022-02-08 19:08:27 +0100

    Release 0.0.6 Fixed Bugs

--- src/main/java/me/kacpersdev/Venom.java
+++ src/main/java/me/kacpersdev/Venom.java
@@ -4,33 +4,13 @@ import me.kacpersdev.api.API;
 import me.kacpersdev.api.config.ConfigFile;
 import me.kacpersdev.api.inventory.VenomInventory;
 import me.kacpersdev.chatcolor.CC;
-import me.kacpersdev.listener.InventoryClick;
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.PluginManager;
-import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Venom extends JavaPlugin {
+public final class Venom {
 
     private ConfigFile configFile;
     private VenomInventory venomInventory;
     private API api;
     private CC cc;
-
-    @Override
-    public void onEnable() {
-        this.pluginListeners();
-    }
-
-    @Override
-    public void onDisable() {
-
-    }
-
-    private void pluginListeners(){
-
-        PluginManager manager = Bukkit.getPluginManager();
-        manager.registerEvents(new InventoryClick(), this);
-    }
 
     public ConfigFile getConfigFile(){
         return this.configFile;
--- src/main/java/me/kacpersdev/api/config/ConfigFile.java
+++ src/main/java/me/kacpersdev/api/config/ConfigFile.java
@@ -10,12 +10,17 @@ import java.io.IOException;
 public class ConfigFile {
 
     private final Venom venom = new Venom();
+    private File file;
+    private FileConfiguration configuration;
 
     public void createConfig(File file, FileConfiguration configuration, String dir){
 
+        this.file = file;
+        this.configuration = configuration;
+
         if (!(file.exists())) {
             file.getParentFile().mkdir();
-            venom.saveResource(dir, false);
+
         }
 
         try {
@@ -25,5 +30,29 @@ public class ConfigFile {
         } catch (InvalidConfigurationException e) {
             e.printStackTrace();
         }
+    }
+
+    public void save(){
+
+        try {
+            configuration.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void load(){
+
+        try {
+            configuration.load(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getString(String path){
+        return this.configuration.getString(path);
     }
 }
