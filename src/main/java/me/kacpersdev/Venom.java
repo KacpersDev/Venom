16793ac6f75d38472125cc0986984163826c6e96
kacper <vkacperpl@gmail.com>
2022-02-08 19:21:22 +0100

    Release 0.0.9 Fixed Bugs

--- src/main/java/me/kacpersdev/Venom.java
+++ src/main/java/me/kacpersdev/Venom.java
@@ -1,20 +1,14 @@
 package me.kacpersdev;
 
 import me.kacpersdev.api.API;
-import me.kacpersdev.api.config.ConfigFile;
 import me.kacpersdev.api.inventory.VenomInventory;
 import me.kacpersdev.chatcolor.CC;
 
 public final class Venom {
 
-    private final ConfigFile configFile = new ConfigFile();
     private final VenomInventory venomInventory = new VenomInventory();
     private final API api = new API();
     private final CC cc = new CC();
-
-    public ConfigFile getConfigFile(){
-        return this.configFile;
-    }
 
     public VenomInventory getVenomInventory(){
         return this.venomInventory;
