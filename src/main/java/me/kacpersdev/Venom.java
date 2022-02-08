ba6498e800637a5a38198240a6c78f34d796553b
kacper <vkacperpl@gmail.com>
2022-02-08 22:26:57 +0100

    Release 0.1.1 Fixed all issues. Inventory feature has been temp disabled.

--- src/main/java/me/kacpersdev/Venom.java
+++ src/main/java/me/kacpersdev/Venom.java
@@ -1,20 +1,14 @@
 package me.kacpersdev;
 
 import me.kacpersdev.api.API;
-import me.kacpersdev.api.inventory.VenomInventory;
 import me.kacpersdev.chatcolor.CC;
 import me.kacpersdev.item.Item;
 
 public final class Venom {
 
-    private final VenomInventory venomInventory = new VenomInventory();
     private final API api = new API();
     private final CC cc = new CC();
     private final Item item = new Item();
-
-    public VenomInventory getVenomInventory(){
-        return this.venomInventory;
-    }
 
     public CC getCC(){
         return this.cc;
